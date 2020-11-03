8262ba773660fed94fe8441a2376663e79964ec7
nancy <nancy@xgmail.com>
2020-11-03 17:00:35 -0500

    tank is working.  one tank and fire

--- tank1/src/com/mashibing/tank/Tank.java
+++ tank1/src/com/mashibing/tank/Tank.java
@@ -12,11 +12,14 @@ public class Tank {
    private int x, y;
    private Dir dir=Dir.DOWN;
    private static final int SPEED=10;
-   public Tank(int x, int y, Dir dir){
+   private boolean moving =  false;
+   private TankFrame tf= null;
+   public Tank(int x, int y, Dir dir, TankFrame tf){
        super();
        this.x=x;
        this.y=y;
        this.dir=dir;
+       this.tf=tf;
    }
 
     public Dir getDir() {
@@ -27,9 +30,27 @@ public class Tank {
         this.dir = dir;
     }
 
+    public boolean isMoving() {
+        return moving;
+    }
+
+    public void setMoving(boolean moving) {
+        this.moving = moving;
+    }
+
     public void paint(Graphics g){
-       g.fillRect(x, y, 50, 50);
+        Color c= g.getColor();
+        g.setColor(Color.YELLOW);
+        g.fillRect(x, y, 50, 50);
+        g.setColor(c);
+
+
        //System.out.println("paint");
+        move();
+
+   }
+   public void move(){
+       if(!moving) return;
        switch(dir){
            case LEFT:
                x-=SPEED;
@@ -44,6 +65,11 @@ public class Tank {
                y+=SPEED;
                break;
        }
+   }
+
+   public void fire(){
+       tf.bullets.add(new Bullet(this.x, this.y, this.dir, this.tf));
+
    }
 
 }
